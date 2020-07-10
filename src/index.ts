9d3636f10fd6c2470047a60a276f5787361bc5e1
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2020-07-10 08:24:41 -0400

    Initial

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import WebSocket from "ws";
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (socket, req) => {
+    console.log(`Received new connection from ${req.connection.remoteAddress}`);
+
+    socket.on("message", (data) => {
+        try {
+            const obj = JSON.parse(data.toString());
+            if (obj.topic === "digital-write") {
+                const data = JSON.stringify({
+                    topic: "digital-read",
+                    payload: {
+                        channel: 0,
+                        value: obj.payload.value
+                    }
+                });
+                socket.send(data);
+            }
+        }
+        catch (err) {
+            console.log("Error parsing: ", err);
+        }
+        console.log(`Received: ${data.toString()}`);
+    });
+});
